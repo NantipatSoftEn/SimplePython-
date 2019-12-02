a043a7ea22cc52d6d1dafee828eee23596da30d0
Army <keymasterviriya1150@gmail.com>
2019-12-02 13:49:32 +0700

    traning

---
+++ condition.py
@@ -0,0 +1,16 @@
+n = int(5)
+
+if (n > 0):
+    print ('x is positive number')
+    print ('Show number from 0 to %d' % (n - 1))
+
+else:
+    print ('x isn\'t positive number')
+
+# for i in range(n):
+#    print(i)
+
+
+name = "John"
+if name in ["John", "Rick"]:
+    print("Your name is either John or Rick.")
