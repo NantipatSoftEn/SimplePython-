c45ae3c7ebdcc3e81d95d3389eb51dd726bbf80e
Army <keymasterviriya1150@gmail.com>
2019-12-02 14:52:41 +0700

    playground

---
+++ class.py
@@ -0,0 +1,8 @@
+class Charecter:
+    int = 20
+
+    def run(self):
+        print("run")
+
+obj = Charecter();
+obj.run();
--- condition2.py
+++ condition2.py
@@ -1,11 +1,8 @@
 statement = False
 another_statement = True
 if statement is True:
-    # do something
     pass
 elif another_statement is True: # else if
-    # do something else
     pass
 else:
-    # do another thing
     pass
