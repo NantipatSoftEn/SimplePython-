db8d29844f5d940fc13ee46f4bacc103a4bec00a
Army <keymasterviriya1150@gmail.com>
2019-12-02 10:24:24 +0700

    string

---
+++ stringformat.py
@@ -0,0 +1,17 @@
+data = ("John", "Doe", 53.44)
+format_string = "Hello %s %s. Your current balance is $%s."
+
+print(format_string % data)
+
+astring = "Hello world!"
+print("single quotes are ' '")
+
+print(len(astring))
+
+
+astring = "Hello world!"
+print(astring.index("o"))
+
+# 
+astring = "armylovefha"
+print(astring[3:7])
